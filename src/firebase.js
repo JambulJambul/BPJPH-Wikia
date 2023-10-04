d61b1d305ad48032c24a1502f3f29fe9ee0d3fb2
Delos-343 <mfachry.handoko@gmail.com>
2023-10-04 14:34:24 +0700

    Hid admin data

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -8,13 +9,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDiyvs3JwuJk_kdOq0FLTi6poOaPqBOhH0",
-  authDomain: "kamus-bpjph.firebaseapp.com",
-  projectId: "kamus-bpjph",
-  storageBucket: "kamus-bpjph.appspot.com",
-  messagingSenderId: "950541522950",
-  appId: "1:950541522950:web:bded6e6fa079e9e0174b87",
-  measurementId: "G-8355F0YMXE"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
